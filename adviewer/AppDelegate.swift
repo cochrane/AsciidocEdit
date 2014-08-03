699c26eaccd11282b1a06ab421eaa1d14dabc5af
James Carlson <jxxcarlson@mac.com>
2014-08-03 08:01:19 -0400

    Initial Commit

---
+++ adviewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  adviewer
+//
+//  Created by James Carlson on 8/3/14.
+//  Copyright (c) 2014 James Carlson. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ adviewer/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  adviewer
+//
+//  Created by James Carlson on 8/3/14.
+//  Copyright (c) 2014 James Carlson. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
