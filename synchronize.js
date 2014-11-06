a51c1c9db215b1f88d8b3134fb8c6f1ee3163ec0
James Carlson <jxxcarlson@mac.com>
2014-11-06 18:18:25 +0800

    Added image to README

--- synchronize.js
+++ synchronize.js
@@ -8,32 +8,39 @@ function keep_place() {
     var position = localStorage.getItem("scroll_position");
     
     $("body").scrollTop(position)
-
+    
 };
 
 $(document).ready(function(){
-
-  $("#b1").click(function(){
-	 
-	keep_place()
-
-  });
+                  
+                  $("#b1").click(function(){
+                                 
+                                 keep_place()
+                                 
+                                 });
+                  
+                  });
 
 $(document).ready(function(){
+                  
+                  $("#b2").click(function(){
+                                 
+                                 $("#f2").html("foobar")
+                                 
+                                 });
+                  
+                  });
 
-    $(window).scroll(function(){
+$(document).ready(function(){
+                  
+                  $(window).scroll(function(){
+                                   
+                                   var scroll_position = $("body").scrollTop();
+                                   
+                                   localStorage.setItem("scroll_position", scroll_position);   
+                                   })
+                  });
 
-       var scroll_position = $("body").scrollTop();
-
-       localStorage.setItem("scroll_position", scroll_position);
- 
-    
-    })
-
-});
-
- 
-});
 
 </script>
 
